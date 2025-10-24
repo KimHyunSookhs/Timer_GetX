8ca01e2e9f977beb3ae7b7c18f5dbbedf6fd3678
unknown <rlagustn070@naver.com>
2025-10-24 11:37:08 +0900

    feat : 타이머 시작, 멈춤 기능 추가

--- lib/controller/timer_controller.dart
+++ lib/controller/timer_controller.dart
@@ -5,9 +5,28 @@ import 'package:stop_watch/timer_status.dart';
 
 class TimerController extends GetxController{
   final RxInt seconds = 0.obs;
-  final RxBool isRunning = false.obs;
+  final isRunning = false.obs;
   final timerStatus = <TimerStatus>[].obs;
-
+  static const maxSecond = 2500;
+  int remainingSecond = maxSecond;
   late Timer timer;
+
+  void startTimer(){
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      if(remainingSecond >0){
+        remainingSecond--;
+      }else {
+        timer.cancel();
+      }
+    });
+  }
+
+  void pausedTimer(){
+    if(isRunning.value){
+         timer.cancel();
+    }else{
+      startTimer();
+    }
+  }
 
 }
